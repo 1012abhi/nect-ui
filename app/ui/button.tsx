1f698573893be5d06e4e967b21b14271b50a8805
1012abhi <130040653+1012abhi@users.noreply.github.com>
2026-04-03 19:30:30 +0530

    Add Button component and utility function; update dependencies

---
+++ app/ui/button.tsx
@@ -0,0 +1,9 @@
+export default function Button() {
+    return (
+        <>
+            <button className="bg-[#101010] hover:bg-[#202020] text-white font-normal py-2 px-8 rounded-full transform transition-opacity">
+                Button
+            </button>
+        </>
+    )
+}
